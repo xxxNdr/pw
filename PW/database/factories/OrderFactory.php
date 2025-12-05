56d8a8fe9bb1bdcb9ce53ff8516cb45f8eb0223a
xxxndr <andrea1987andre@gmail.com>
2025-12-05 23:49:20 +0100

    creati Facory e Seeder per Order

--- PW/database/factories/OrderFactory.php
+++ PW/database/factories/OrderFactory.php
@@ -4,20 +4,20 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
- */
 class OrderFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+        // Restituisce array con dati finti per ogni colonna della migration
         return [
-            //
+            'nome-cliente' => $this->faker->name(),
+            'email-cliente' => $this->faker->safeEmail(),
+            // true = unisci le parole dell'array in un'unica stringa
+            'nome-prodotto' => $this->faker->words(2, true),
+            'quantita'=>$this->faker->numberBetween(1,10),
+            'descrizione'=>$this->faker->paragraph(2),
+            'prezzo'=>$this->faker->randomFloat(2,10,5000),
+            'spedito'=>$this->faker->boolean()
         ];
     }
 }
--- PW/database/seeders/OrderSeeder.php
+++ PW/database/seeders/OrderSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Order;
 
 class OrderSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Order::factory(10)->create();
     }
 }
