c3bd8ad55c74021227ec2a4d241ec633af1d465d
xxxndr <andrea1987andre@gmail.com>
2025-12-05 15:24:38 +0100

    homepage compilata, easy, minimal

---
+++ PW/resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<html lang="it">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Projet Work</title>
+    {{-- collegamento laravel react/tailwind --}}
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+</head>
+
+<body class="bg-gray-100">
+    <div id="app"></div>
+</body>
+
+</html>
