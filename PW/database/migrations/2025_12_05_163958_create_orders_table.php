54ed55ee3360b85a3ad23987f4e0e60324141657
xxxndr <andrea1987andre@gmail.com>
2025-12-05 17:50:21 +0100

    creata migration per Order

--- PW/database/migrations/2025_12_05_163958_create_orders_table.php
+++ PW/database/migrations/2025_12_05_163958_create_orders_table.php
@@ -6,20 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('nome');
+            $table->integer('quantita');
+            $table->text('descrizione');
+            $table->decimal('prezzo', 6, 2);
+            $table->boolean('in_produzione')->default(true);
+            $table->timestamps('creato_il');
+            $table->timestamps('modificato_il');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('orders');
