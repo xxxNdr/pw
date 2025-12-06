743c38c1286c47e6fd5bb97df80759080c95df6e
xxxndr <andrea1987andre@gmail.com>
2025-12-06 01:05:37 +0100

    sfondo animato gradiente

--- PW/resources/js/app.jsx
+++ PW/resources/js/app.jsx
@@ -1,12 +1,14 @@
 // importa createRoot da react-dom/client per montare l'app
 import { createRoot } from "react-dom/client";
+// importa un po' di css
+import "../css/stile.css";
 // seleziona l'elemento html dal DOM in welcome.blade.php
 createRoot(document.getElementById("app"))
     // con id app e sostituisci il contenuto con ↓
     .render(
         /* div che occupa almeno tutta l'altezza dello schermo
         gradiente da sx a dx */
-        <div className="min-h-screen bg-radial at-center from-purple-400 to-pink-950 flex items-center justify-center text-white">
+        <div className="min-h-screen anime flex items-center justify-center text-white">
             {/* contenitore centrato per il testo */}
             <div className="text-center">
                 <h1 className="text-9xl font-bold text-blue-500 filter drop-shadow-[0_4px_6px_rgb(0,0,0)]">
